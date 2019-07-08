2c9a55eb59dfff18f10a4e5d62ac7e4ce7a70b45
Bolilyy1984 <46760967+Bolilyy1984@users.noreply.github.com>
2019-07-08 18:07:34 +0300

    Add files via upload

---
+++ ДЗ№2 задание 4 Math.py
@@ -0,0 +1,25 @@
+
+# 4. Задание Свойства логарифмов. Вычислить:
+
+import math
+
+a = math.log(16, 4)
+print(a)
+b = math.log(1/25, 5)
+print(b)
+c = math.log(5, 25)
+print(c)
+d = math.log(math.sqrt(27), 3)
+print(d)
+e = math.log(12, 2) - math.log(3, 2)
+print(e)
+f = math.log(12, 6) - math.log(3, 6)
+print(f)
+g = math.exp(math.log(5))
+print(g)
+h = math.log(225, 2) / math.log(15, 2)
+print(h)
+i = math.log(32, 4) + math.log(10, 0.1)
+print(i)
+j = 9 ** (math.log(math.sqrt(5), 3))
+print(j)
